b88bd0b2120ed40c78a382c02da78b77b92cdd68
yousseflouhaichi <youssef.louhaichi@esprit.tn>
2020-12-03 17:18:39 +0100

    add loginPHP

---
+++ Front/login.php
@@ -0,0 +1,37 @@
+<?php
+include_once 'includes/DBconnection.php';
+
+$email = $_POST['email'];
+$mdp = $_POST['mdp'];
+
+$sql="SELECT * from utilisateurs where email='$email';";
+$result=mysqli_query($conn,$sql);
+if(mysqli_num_rows($result)==0) 
+{
+    echo("Email invalide");
+}
+else
+{
+    $sql="SELECT * from utilisateurs where email='$email' and mdp='$mdp';";
+    $result=mysqli_query($conn,$sql);
+    if(mysqli_num_rows($result)==0) 
+    {
+      echo("Mot de passe invalide");
+    }
+    else
+    {
+    $sql="SELECT * from utilisateurs where email='$email' and mdp='$mdp' and ban=0";
+    $result=mysqli_query($conn,$sql);
+    if(mysqli_num_rows($result)==0) 
+    {
+      echo("Votre compte est suspendu");
+    }
+    else
+    {
+        $sql="SELECT nom from utilisateurs where email='$email' and mdp='$mdp';";
+        $nom=mysqli_query($conn,$sql);
+        echo( "bienvenu" );
+    }
+}
+
+}
