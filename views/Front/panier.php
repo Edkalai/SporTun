6e56b75ea6c5a2ed7e1654cbfba560382545dc89
Ahmed Kalai <ahmedkalai1@esprit.tn>
2020-12-15 19:05:44 +0100

    added balance & payment

--- views/Front/panier.php
+++ views/Front/panier.php
@@ -20,6 +20,7 @@ $somme=mysqli_fetch_assoc($req);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SporTun</title>
+    <link rel="shortcut icon" href="assets/img/logo.ico">
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
@@ -89,7 +90,7 @@ $somme=mysqli_fetch_assoc($req);
                   </tr>
                 </table>
             </div>
-            <button  class="btn checkout-btn"><a href="logout.php" style="color:white;" > Payer </a> </button> 
+            <button  class="btn checkout-btn"><a href="payer.php" style="color:white;" > Payer </a> </button> 
         </div>
     </div>
   
